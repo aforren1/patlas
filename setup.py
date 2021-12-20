6668f3b03df06cf43ebd9226f14bd1f4aaaae573
Alex Forrence <alex.forrence@gmail.com>
2021-12-20 10:47:31 -0500

    retry conditional OpenMP

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ from setuptools.extension import Extension
 eca = []
 ela = []
 
-#if environ.get('OMP'):
-if sys.platform == 'win32':
-    eca.append('/openmp')
-    ela.append('/openmp')
-else:
-    if sys.platform == 'darwin':
-        eca.append('-Xpreprocessor')
-        ela.append('-lomp')
+if environ.get('OMP'):
+    if sys.platform == 'win32':
+        eca.append('/openmp')
+        ela.append('/openmp')
     else:
-        ela.append('-fopenmp')
-    
-    eca.append('-fopenmp')
+        if sys.platform == 'darwin':
+            eca.append('-Xpreprocessor')
+            ela.append('-lomp')
+        else:
+            ela.append('-fopenmp')
+        
+        eca.append('-fopenmp')
 
 ext = [Extension('patlas',
                  sources=['patlas.pyx'],
